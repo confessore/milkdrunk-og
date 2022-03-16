0bcba14b63e9740aca4644de75a8ad72f91dca10
confessore <confessore@balasolu.com>
2022-03-16 03:25:42 -0400

    new layout

--- milkdrunk/views/NewSleepingPage.cs
+++ milkdrunk/views/NewSleepingPage.cs
@@ -16,37 +16,58 @@ namespace milkdrunk.views
             return new StackLayout()
             {
                 Children = {
-                    new Label() { Text = "new sleeping" }
-                        .Margins(5, 5, 5, 5)
-                        .Paddings(5, 5, 5, 5)
-                        .CenterHorizontal(),
-                    new Label() { Text = "start" }
-                        .Margins(5, 5, 5, 5)
-                        .Paddings(5, 5, 5, 5),
-                    new DatePicker()
-                        .Margins(5, 5, 5, 5),
-                    new TimePicker()
-                        .Margins(5, 5, 5, 5),
-                    new Label() { Text = "log end now?"}
-                        .Margins(5, 5, 5, 5)
-                        .Paddings(5, 5, 5, 5),
-                    new CheckBox()
-                        .Margins(5, 5, 5, 5)
-                        .Bind(CheckBox.IsCheckedProperty, nameof(_vm.IsChecked)),
-                    new Label() { Text = "end" }
-                        .Margins(5, 5, 5, 5)
-                        .Paddings(5, 5, 5, 5)
-                        .Bind(Label.IsVisibleProperty, nameof(_vm.IsChecked)),
-                    new DatePicker()
-                        .Margins(5, 5, 5, 5)
-                        .Bind(DatePicker.IsVisibleProperty, nameof(_vm.IsChecked)),
-                    new TimePicker()
-                        .Margins(5, 5, 5, 5)
-                        .Bind(DatePicker.IsVisibleProperty, nameof(_vm.IsChecked)),
-                    new Button() { Text = AppResources.button_confirm }
-                        .Margins(5, 5, 5, 5)
-                        .Paddings(5, 5, 5, 5),
-                        //.Bind(Button.CommandProperty, nameof(_vm.MyBabiesCommand)),
+                    new StackLayout()
+                    {
+                        VerticalOptions = LayoutOptions.Start,
+                        Children =
+                        {
+                            new Label() { Text = "new sleeping" }
+                                .Margins(5, 5, 5, 5)
+                                .Paddings(5, 5, 5, 5)
+                                .CenterHorizontal()
+                        }
+                    },
+                    new StackLayout()
+                    {
+                        VerticalOptions = LayoutOptions.StartAndExpand,
+                        Children =
+                        {
+                            new Label() { Text = "start" }
+                                .Margins(5, 5, 5, 5)
+                                .Paddings(5, 5, 5, 5),
+                            new DatePicker()
+                                .Margins(5, 5, 5, 5),
+                            new TimePicker()
+                                .Margins(5, 5, 5, 5),
+                            new Label() { Text = "log end now?"}
+                                .Margins(5, 5, 5, 5)
+                                .Paddings(5, 5, 5, 5),
+                            new CheckBox()
+                                .Margins(5, 5, 5, 5)
+                                .Bind(CheckBox.IsCheckedProperty, nameof(_vm.IsChecked)),
+                            new Label() { Text = "end" }
+                                .Margins(5, 5, 5, 5)
+                                .Paddings(5, 5, 5, 5)
+                                .Bind(Label.IsVisibleProperty, nameof(_vm.IsChecked)),
+                            new DatePicker()
+                                .Margins(5, 5, 5, 5)
+                                .Bind(DatePicker.IsVisibleProperty, nameof(_vm.IsChecked)),
+                            new TimePicker()
+                                .Margins(5, 5, 5, 5)
+                                .Bind(DatePicker.IsVisibleProperty, nameof(_vm.IsChecked))
+                        }
+                    },
+                    new StackLayout()
+                    {
+                        VerticalOptions = LayoutOptions.End,
+                        Children =
+                        {
+                            new Button() { Text = AppResources.button_confirm }
+                                .Margins(5, 5, 5, 5)
+                                .Paddings(5, 5, 5, 5),
+                                //.Bind(Button.CommandProperty, nameof(_vm.MyBabiesCommand))
+                        }
+                    }
                 }
             };
         }
